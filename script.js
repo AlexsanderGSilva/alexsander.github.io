d0a2bdfaa250d92dc13883c2ceeafcdb08d6ec39
AlexsanderGSilva <alexsander.gon.silva@gmail.com>
2024-12-09 18:53:06 -0300

    commit 6

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .portifolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .portifolio-box', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
